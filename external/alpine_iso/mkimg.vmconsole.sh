3b2bff585273c15259fd2d2c437d7afb72df512b
keyner arismendy <131421793+DesarrolladorPrimary@users.noreply.github.com>
2026-04-23 02:04:34 -0500

    Force serial console and vmConsole overlay in Alpine profile

--- external/alpine_iso/mkimg.vmconsole.sh
+++ external/alpine_iso/mkimg.vmconsole.sh
@@ -5,9 +5,11 @@ profile_vmconsole() {
 	hostname="vmconsole"
 	image_name="alpine"
 	output_filename="alpine-x86_64.iso"
-	kernel_cmdline="console=ttyS0,115200 console=tty0"
+	initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage"
+	kernel_cmdline="console=ttyS0,115200n8 earlyprintk=serial,ttyS0,115200"
 	syslinux_serial="0 115200"
 	syslinux_timeout="20"
 	syslinux_prompt="1"
+	apkovl="genapkovl-vmconsole.sh"
 	apks="$apks openssh-server"
 }
