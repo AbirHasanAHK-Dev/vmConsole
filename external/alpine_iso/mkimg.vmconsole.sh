784053bef700c5fba3aecb9613c7cd0982b56f0c
keyner arismendy <131421793+DesarrolladorPrimary@users.noreply.github.com>
2026-04-23 19:32:31 -0500

    Force vmConsole Alpine kernel to serial console

--- external/alpine_iso/mkimg.vmconsole.sh
+++ external/alpine_iso/mkimg.vmconsole.sh
@@ -1,12 +1,13 @@
 profile_vmconsole() {
 	profile_base
 	arch="x86_64"
+	kernel_flavors="virt"
 	profile_abbrev="vmc"
 	hostname="vmconsole"
 	image_name="alpine"
 	output_filename="alpine-x86_64.iso"
-	initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage"
-	kernel_cmdline="console=ttyS0,115200n8 earlyprintk=serial,ttyS0,115200"
+	initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage,virtio_pci,virtio_scsi,virtio_blk,virtio_net"
+	kernel_cmdline="console=ttyS0,115200n8 console=tty0 earlycon=uart,io,0x3f8,115200 earlyprintk=serial,ttyS0,115200 ignore_loglevel loglevel=8 panic=30"
 	syslinux_serial="0 115200"
 	syslinux_timeout="20"
 	syslinux_prompt="1"
