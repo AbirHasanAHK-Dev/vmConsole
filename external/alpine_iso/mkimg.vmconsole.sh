977662a44c70153d11eeed7c2942c703c61768cc
keyner arismendy <131421793+DesarrolladorPrimary@users.noreply.github.com>
2026-04-23 23:02:06 -0500

    Keep serial console last in vmConsole Alpine kernel cmdline

--- external/alpine_iso/mkimg.vmconsole.sh
+++ external/alpine_iso/mkimg.vmconsole.sh
@@ -7,7 +7,7 @@ profile_vmconsole() {
 	image_name="alpine"
 	output_filename="alpine-x86_64.iso"
 	initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage,virtio_pci,virtio_scsi,virtio_blk,virtio_net"
-	kernel_cmdline="console=ttyS0,115200n8 console=tty0 earlycon=uart,io,0x3f8,115200 earlyprintk=serial,ttyS0,115200 ignore_loglevel loglevel=8 panic=30"
+	kernel_cmdline="nomodeset vga=normal console=tty0 console=ttyS0,115200n8 earlycon=uart,io,0x3f8,115200 earlyprintk=serial,ttyS0,115200 ignore_loglevel loglevel=8 panic=30"
 	syslinux_serial="0 115200"
 	syslinux_timeout="20"
 	syslinux_prompt="1"
